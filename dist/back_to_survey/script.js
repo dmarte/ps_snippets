95548a9ab58bac56908a9edd82766bf990b61c12
Delvi Marte <delvi.marte@gmail.com>
2022-08-12 15:59:52 -0400

    Minor lib updates

--- dist/back_to_survey/script.js
+++
@@ -1,8 +0,0 @@
-import { PadSquadDOM } from './../PadSquadDOM';
-(async (w) => {
-    console?.info('-- BACK TO SURVEY SCRIPT --');
-    console.log(PadSquadDOM.find('#simpli-vplacement-'))
-    const element = await PadSquadDOM.when('#simpli-vplacement-');
-    console.log('ELEMENT: ---> ', element);
-})(window);
-//# sourceMappingURL=script.js.map
