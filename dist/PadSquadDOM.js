21bc383e0fd4e91c93e9fa1f7cc477064c4d4e10
Delvi Marte <delvi.marte@gmail.com>
2022-08-12 15:51:22 -0400

    Minor lib updates

--- dist/PadSquadDOM.js
+++ dist/PadSquadDOM.js
@@ -1,45 +1,31 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+export class PadSquadDOM {
+    static findOrFail(selector) {
+        const element = PadSquadDOM.find(selector);
+        if (!element) {
+            throw new DOMException(`No elements found for ${selector}.`);
+        }
+        return element;
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
+    static find(selector) {
+        return document.querySelector(selector);
     }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.PadSquadDOM = void 0;
-    class PadSquadDOM {
-        static findOrFail(selector) {
-            const element = PadSquadDOM.find(selector);
-            if (!element) {
-                throw new DOMException(`No elements found for ${selector}.`);
+    static when(selector) {
+        return new Promise((resolve, reject) => {
+            try {
+                const element = PadSquadDOM.findOrFail(selector);
+                resolve(element);
             }
-            return element;
-        }
-        static find(selector) {
-            return document.querySelector(selector);
-        }
-        static when(selector) {
-            return new Promise((resolve, reject) => {
-                try {
-                    const element = PadSquadDOM.findOrFail(selector);
+            catch (error) {
+                const observer = new MutationObserver(() => {
+                    const element = PadSquadDOM.find(selector);
+                    if (!element) {
+                        return;
+                    }
                     resolve(element);
-                }
-                catch (error) {
-                    const observer = new MutationObserver(() => {
-                        const element = PadSquadDOM.find(selector);
-                        if (!element) {
-                            return;
-                        }
-                        resolve(element);
-                    });
-                    observer.observe(document.body, { childList: true, subtree: true });
-                }
-            });
-        }
+                });
+                observer.observe(document.body, { childList: true, subtree: true });
+            }
+        });
     }
-    exports.PadSquadDOM = PadSquadDOM;
-});
+}
 //# sourceMappingURL=PadSquadDOM.js.map
--- dist/back_to_survey/script.js
+++ dist/back_to_survey/script.js
@@ -1,28 +1,8 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./../PadSquadDOM"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    const PadSquadDOM_1 = require("./../PadSquadDOM");
-    ((w) => __awaiter(void 0, void 0, void 0, function* () {
-        console === null || console === void 0 ? void 0 : console.info('-- BACK TO SURVEY SCRIPT --');
-        const element = yield PadSquadDOM_1.PadSquadDOM.when('#simpli-vplacement-');
-        console.log('ELEMENT: ---> ', element);
-    }))(window);
-});
+import { PadSquadDOM } from './../PadSquadDOM';
+(async (w) => {
+    console?.info('-- BACK TO SURVEY SCRIPT --');
+    console.log(PadSquadDOM.find('#simpli-vplacement-'))
+    const element = await PadSquadDOM.when('#simpli-vplacement-');
+    console.log('ELEMENT: ---> ', element);
+})(window);
 //# sourceMappingURL=script.js.map
