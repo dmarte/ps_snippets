6644357cb1aa5b9617c18eacc7b70e26358f3c56
Moises Martinez <moises@padsquad.com>
2022-08-15 15:37:30 -0400

    moises update

--- src/WeatherRenderig/WeatherRenderign.js
+++ src/WeatherRenderig/WeatherRenderign.js
@@ -1,4 +1,7 @@
 window.addEventListener('load', () => { 
     console.log('simpli...');
     console.log(__simpli.runtime().environment);
+    __simpli.runtime().environment.outerWrapper.parentNode.parentNode.style.margin = '12px 0px';
+    __simpli.runtime().environment.outerWrapper.parentNode.parentNode.style.visibility = 'visible';
+    
 })
