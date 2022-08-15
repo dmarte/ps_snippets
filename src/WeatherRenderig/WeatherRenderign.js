38ccac39d9eb6cde0fa56b85f2c149bff71a8106
Moises Martinez <moises@padsquad.com>
2022-08-15 17:38:24 -0400

    moises update

--- src/WeatherRenderig/WeatherRenderign.js
+++ src/WeatherRenderig/WeatherRenderign.js
@@ -1,19 +1,18 @@
 window.addEventListener('load', () => { 
 
-    console.log('hi fron script ap');
+    const simpliTagEnvouterWrapper = __simpli.runtime().environment.outerWrapper;
 
-    if(__simpli.runtime().environment.outerWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb')){
-        __simpli.runtime().environment.outerWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb').style.visibility = 'visible';
-        __simpli.runtime().environment.outerWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb').style.margin = '12px 0px';
-        __simpli.runtime().environment.outerWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb').style.zIndex = '100 !important';
+    if(simpliTagEnvouterWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb')){
+
+        simpliTagEnvouterWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb').style.visibility = 'visible';
+        simpliTagEnvouterWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb').style.margin = '12px 0px';
+        simpliTagEnvouterWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb').style.zIndex = '100 !important';
     
     }
     
-    if(__simpli.runtime().environment.outerWrapper.closest('.Card--card--HiWPW')){
-        __simpli.runtime().environment.outerWrapper.closest('.Card--card--HiWPW').style.visibility = 'visible';
-        __simpli.runtime().environment.outerWrapper.closest('.Card--card--HiWPW').style.margin = '12px 0px';
-    }
+    if(simpliTagEnvouterWrapper.closest('.Card--card--HiWPW')){
+        simpliTagEnvouterWrapper.closest('.Card--card--HiWPW').style.visibility = 'visible';
+        simpliTagEnvouterWrapper.closest('.Card--card--HiWPW').style.margin = '12px 0px';
+    }  
 
-    
-    
 })
