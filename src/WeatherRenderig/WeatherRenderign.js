28348dd8ea6e4d92eac4f3a0244e66abc710f349
Moises Martinez <moises@padsquad.com>
2022-08-15 17:06:05 -0400

    moises update

--- src/WeatherRenderig/WeatherRenderign.js
+++ src/WeatherRenderig/WeatherRenderign.js
@@ -5,11 +5,15 @@ window.addEventListener('load', () => {
     if(__simpli.runtime().environment.outerWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb')){
         __simpli.runtime().environment.outerWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb').style.visibility = 'visible';
         __simpli.runtime().environment.outerWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb').style.margin = '12px 0px';
+        __simpli.runtime().environment.outerWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb').style.zIndex = 100;
+    
     }
     
     if(__simpli.runtime().environment.outerWrapper.closest('.Card--card--HiWPW')){
         __simpli.runtime().environment.outerWrapper.closest('.Card--card--HiWPW').style.visibility = 'visible';
         __simpli.runtime().environment.outerWrapper.closest('.Card--card--HiWPW').style.margin = '12px 0px';
     }
+
+    
     
 })
