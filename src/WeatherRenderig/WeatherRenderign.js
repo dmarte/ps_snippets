fa7fc11cbfd9302e56a49eca85afebdc75435864
Moises Martinez <moises@padsquad.com>
2022-08-15 16:38:00 -0400

    moises update

--- src/WeatherRenderig/WeatherRenderign.js
+++ src/WeatherRenderig/WeatherRenderign.js
@@ -1,16 +1,12 @@
 window.addEventListener('load', () => { 
-   // __simpli.runtime().environment.outerWrapper.parentNode.parentNode.style.margin = '12px 0px';
-    // __simpli.runtime().environment.outerWrapper.parentNode.parentNode.style.visibility = 'visible';
+
     if(__simpli.runtime().environment.outerWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb')){
         __simpli.runtime().environment.outerWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb').style.visibility = 'visible';
-    }else{
-        console.log('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb NOT');
+        __simpli.runtime().environment.outerWrapper.closest('.BaseAd--adWrapper--2qtTX.BaseAd--card--1NlKb').style.margin = '12px 0px';
     }
     
     if(__simpli.runtime().environment.outerWrapper.closest('.Card--card--HiWPW')){
-        __simpli.runtime().environment.outerWrapper.closest('.Card--card--HiWPW').style.margin = "20px 0px"; 
-    }else{
-        console.log('.Card--card--HiWPW NOT');
+        __simpli.runtime().environment.outerWrapper.closest('.Card--card--HiWPW').style.visibility = 'visible';
     }
     
 })
